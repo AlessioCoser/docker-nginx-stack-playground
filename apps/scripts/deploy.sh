ff9d9417f0e1ca61c6824754dc262ff73bd15c42
Alessio Coser <alessio.coser@xpeppers.com>
2018-12-12 13:01:05 +0100

    removes auto-generated .tar-gz

--- apps/scripts/deploy.sh
+++ apps/scripts/deploy.sh
@@ -9,6 +9,7 @@ ssh ${REMOTE_SERVER} "mkdir -p /var/apps/${APP}"
 docker build -t local/${APP} ./apps/${APP}
 docker save local/${APP} -o ./apps/${APP}/${APP}.tar.gz
 scp ./apps/${APP}/${APP}.tar.gz ${REMOTE_SERVER}:/var/apps/${APP}/${APP}.tar.gz
+rm ./apps/${APP}/${APP}.tar.gz
 ssh ${REMOTE_SERVER} "docker load --input /var/apps/${APP}/${APP}.tar.gz"
 
 scp ./apps/${APP}/docker-compose.yml ${REMOTE_SERVER}:/var/apps/${APP}/docker-compose.yml
